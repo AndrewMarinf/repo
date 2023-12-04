7652ae0f1c459ace2fd1ec69989ba633e9e0d9df
AndrewMarinf <andrew.marin@bk.ru>
2023-12-04 21:45:11 +0300

    is_ dist_==

--- file__1.py
+++ file__1.py
@@ -1,3 +1,21 @@
 print(111)
 print(222)
 print(555)
+
+a = {'name':'Vasya','surname':'Petrov', 'age':25}
+print(a['surname'])
+
+
+a = {1:'qwe'}
+b = a 
+b[1] = 'asv'
+print(a)
+
+
+c = 500000000000000
+v = 500000000000000
+print(c == v)
+print(c is v)
+
+print(id(c))
+print(id(v))
